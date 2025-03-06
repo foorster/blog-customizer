72033b708401ead40a7191b78d1fd955194dd57e
Olga <o-lin98@mail.ru>
2025-03-06 14:30:04 +0300

    Добавлена возможность закрывать форму при клике вне  сайдбара

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,4 +1,4 @@
-import { useState, FormEvent } from 'react';
+import { useState, FormEvent, useRef, useEffect } from 'react';
 import clsx from 'clsx';
 
 import { ArrowButton } from 'src/ui/arrow-button';
@@ -7,95 +7,117 @@ import { Text } from 'src/ui/text';
 import { Select } from 'src/ui/select';
 import { RadioGroup } from 'src/ui/radio-group';
 import { Separator } from 'src/ui/separator';
-import styles from './ArticleParamsForm.module.scss'; 
+import styles from './ArticleParamsForm.module.scss';
 import {
-    ArticleStateType,
-    OptionType,
-    backgroundColors,
-    contentWidthArr,
-    fontColors,
-    fontFamilyOptions,
-    fontSizeOptions,
-    defaultArticleState,
+	ArticleStateType,
+	OptionType,
+	backgroundColors,
+	contentWidthArr,
+	fontColors,
+	fontFamilyOptions,
+	fontSizeOptions,
+	defaultArticleState,
 } from 'src/constants/articleProps';
 
 interface ArticleStateProps {
-    setArticleState: (states: ArticleStateType) => void; // Функция для обновления состояния
+	setArticleState: (states: ArticleStateType) => void; // Функция для обновления состояния
 }
 
-export const ArticleParamsForm: React.FC<ArticleStateProps> = ({ setArticleState }) => {
-    const [isOpen, setOpen] = useState(false); // Состояние, определяющее, открыт ли сайдбар с настройками
-    const [formState, setFormState] = useState<ArticleStateType>(defaultArticleState); //Состояние формы, содержащее текущие значения параметров статьи
+export const ArticleParamsForm: React.FC<ArticleStateProps> = ({
+	setArticleState,
+}) => {
+	const [isOpen, setOpen] = useState(false); // Состояние, определяющее, открыт ли сайдбар с настройками
+	const [formState, setFormState] =
+		useState<ArticleStateType>(defaultArticleState); //Состояние формы, содержащее текущие значения параметров статьи
 
-    const formChange = (formParameter: string) => (value: OptionType) => {
-        setFormState(prevState => ({ ...prevState, [formParameter]: value }));  // Используем prevState для избежания проблем с асинхронным обновлением состояния
-    };
+	const asideRef = useRef<HTMLElement>(null); // Ссылка на aside
 
-    const formReset = () => {
-        setFormState(defaultArticleState); // Функция для сброса состояния формы к значениям по умолчанию
-    };
+	const formChange = (formParameter: string) => (value: OptionType) => {
+		setFormState((prevState) => ({ ...prevState, [formParameter]: value })); // Используем prevState для избежания проблем с асинхронным обновлением состояния
+	};
 
-    const formSubmit = (event: FormEvent) => {
-        event.preventDefault();
-        setArticleState(formState); // Обновляем состояние статьи с использованием значений из формы
-    };
+	const formReset = () => {
+		setFormState(defaultArticleState); // Функция для сброса состояния формы к значениям по умолчанию
+	};
 
-    return (
-        <>
-            <ArrowButton
-                isOpen={isOpen}
-                onClick={() => setOpen(prev => !prev)} 
-            />
-            <aside
-                className={clsx(styles.container, { [styles.container_open]: isOpen })}
-            >
-                <form className={styles.form} onSubmit={formSubmit}>
-                    <Text size={31} weight={800} uppercase={true}>
-                        Задайте параметры
-                    </Text>
-                    <Select
-                        selected={formState.fontFamilyOption}
-                        options={fontFamilyOptions}
-                        title="Шрифт"
-                        onChange={formChange('fontFamilyOption')}
-                    />
-                    <RadioGroup
-                        name={formState.fontSizeOption.title}
-                        options={fontSizeOptions}
-                        selected={formState.fontSizeOption}
-                        title="Размер шрифта"
-                        onChange={formChange('fontSizeOption')}
-                    />
-                    <Select
-                        selected={formState.fontColor}
-                        options={fontColors}
-                        title="Цвет шрифта"
-                        onChange={formChange('fontColor')}
-                    />
-                    <Separator />
-                    <Select
-                        selected={formState.backgroundColor}
-                        options={backgroundColors}
-                        title="Цвет фона"
-                        onChange={formChange('backgroundColor')}
-                    />
-                    <Select
-                        selected={formState.contentWidth}
-                        options={contentWidthArr}
-                        title="Ширина контента"
-                        onChange={formChange('contentWidth')}
-                    />
-                    <div className={styles.bottomContainer}>
-                        <Button
-                            title="Сбросить"
-                            htmlType="reset"
-                            type="clear"
-                            onClick={formReset}
-                        />
-                        <Button title="Применить" htmlType="submit" type="apply" />
-                    </div>
-                </form>
-            </aside>
-        </>
-    );
+	const formSubmit = (event: FormEvent) => {
+		event.preventDefault();
+		setArticleState(formState); // Обновляем состояние статьи с использованием значений из формы
+	};
+
+	// Обработчик клика вне aside
+	useEffect(() => {
+		const handleClickOutside = (event: MouseEvent) => {
+			if (
+				asideRef.current &&
+				!asideRef.current.contains(event.target as Node)
+			) {
+				setOpen(false); // Закрываем сайдбар
+			}
+		};
+
+		// Добавляем слушатель события при монтировании компонента
+		document.addEventListener('mousedown', handleClickOutside);
+
+		// Удаляем слушатель события при размонтировании компонента
+		return () => {
+			document.removeEventListener('mousedown', handleClickOutside);
+		};
+	}, [asideRef]); // Зависимость от asideRef
+
+	return (
+		<>
+			<ArrowButton isOpen={isOpen} onClick={() => setOpen((prev) => !prev)} />
+			<aside
+				className={clsx(styles.container, { [styles.container_open]: isOpen })}
+				ref={asideRef}>
+				<form className={styles.form} onSubmit={formSubmit}>
+					<Text size={31} weight={800} uppercase={true}>
+						Задайте параметры
+					</Text>
+					<Select
+						selected={formState.fontFamilyOption}
+						options={fontFamilyOptions}
+						title='Шрифт'
+						onChange={formChange('fontFamilyOption')}
+					/>
+					<RadioGroup
+						name={formState.fontSizeOption.title}
+						options={fontSizeOptions}
+						selected={formState.fontSizeOption}
+						title='Размер шрифта'
+						onChange={formChange('fontSizeOption')}
+					/>
+					<Select
+						selected={formState.fontColor}
+						options={fontColors}
+						title='Цвет шрифта'
+						onChange={formChange('fontColor')}
+					/>
+					<Separator />
+					<Select
+						selected={formState.backgroundColor}
+						options={backgroundColors}
+						title='Цвет фона'
+						onChange={formChange('backgroundColor')}
+					/>
+					<Select
+						selected={formState.contentWidth}
+						options={contentWidthArr}
+						title='Ширина контента'
+						onChange={formChange('contentWidth')}
+					/>
+					<div className={styles.bottomContainer}>
+						<Button
+							title='Сбросить'
+							htmlType='reset'
+							type='clear'
+							onClick={formReset}
+						/>
+						<Button title='Применить' htmlType='submit' type='apply' />
+					</div>
+				</form>
+			</aside>
+		</>
+	);
 };
